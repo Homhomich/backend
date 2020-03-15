077f110d84286d73ee213e54b5ef39d252cf19e2
Evgenii Puliaev <puliaevevgenii@gmail.com>
2020-03-15 20:34:15 +0300

    Finished with creating tables for first version of database

--- data/src/main/resources/migrations/changesets/0.1.0/order-010.sql
+++ data/src/main/resources/migrations/changesets/0.1.0/order-010.sql
@@ -1,14 +1,32 @@
-CREATE TABLE breakfast (
+CREATE TABLE breakfasts (
     breakfast_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     tag VARCHAR(50),
     price NUMERIC NOT NULL
 );
 
-CREATE TABLE order (
+CREATE TABLE orders (
     order_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     tag VARCHAR(250),
-    customer_id INTEGER FOREIGN KEY REFERENCES Users(user_id),
-    deliveryman_id INTEGER FOREIGN KEY REFERENCES  Users(user_id)
+    customer_id INTEGER,
+    deliveryman_id INTEGER,
+    CONSTRAINT order_customer_fk FOREIGN KEY (customer_id) REFERENCES users (user_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT order_deliveryman_fk FOREIGN KEY (deliveryman_id) REFERENCES users (user_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE orders_breakfasts (
+    order_breakfast_id SERIAL PRIMARY KEY,
+    order_id INTEGER NOT NULL,
+    breakfast_id INTEGER NOT NULL,
+    CONSTRAINT order_orders_breakfasts_fk FOREIGN KEY (order_id) REFERENCES orders (order_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT breakfasts_orders_breakfasts_fk FOREIGN KEY (breakfast_id) REFERENCES breakfasts (breakfast_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
