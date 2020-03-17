a275d1c21fc923e153f72a6737ad25dd989af1e0
Evgenii Puliaev <puliaevevgenii@gmail.com>
2020-03-17 21:04:45 +0300

    continuing 2

--- src/main/java/ru/relex/tastyfasty/rest/TastyFasty.java
+++ src/main/java/ru/relex/tastyfasty/rest/TastyFasty.java
@@ -3,12 +3,12 @@ package ru.relex.tastyfasty.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import ru.relex.tastyfasty.db.DataConfiguration;
+import ru.relex.tastyfasty.services.ServicesConfiguration;
 
 @SpringBootApplication(
         scanBasePackages = "ru.relex.tastyfasty.rest"
 )
-@Import(DataConfiguration.class)
+@Import(ServicesConfiguration.class)
 public class TastyFasty {
     public static void main(String[] args) {
         SpringApplication.run(TastyFasty.class, args);
