4a41e17cb376f97928e5218af3df9c8e2cf366d5
Evgenii Puliaev <puliaevevgenii@gmail.com>
2020-03-21 09:18:26 +0300

    added status to db, modify orders table

--- data/src/main/java/ru/relex/tastyfasty/db/model/Order.java
+++ data/src/main/java/ru/relex/tastyfasty/db/model/Order.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class Order {
     private int orderID;
+    private int restaurantID;
     private String name;
     private String tag;
     private int customerID;
